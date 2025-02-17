8446d9e7509d943472b34bebe16801b639db67c0
Puttipong Chutipongwanit <puttipong.chu@student.mahidol.edu>
2025-02-17 09:57:46 +0000

    fix format

--- ll.h
+++ ll.h
@@ -128,6 +128,7 @@ void delete_all( LLPtr *sPtr)
       int value = tempPtr->id;
       currentPtr = currentPtr->nextPtr;
       previousPtr->nextPtr = currentPtr;
+      if (previousPtr != NULL) currentPtr->nextPtr->pPtr = previousPtr;
       free( tempPtr );
       printf("delete %d\n", value);
    } // end if
@@ -152,11 +153,11 @@ void printList( LLPtr currentPtr )
 
       // while not the end of the list
       while ( currentPtr->nextPtr!= NULL ) {
-         printf( "%d %s --> ", currentPtr->id, currentPtr->name );
+         printf( "%d %s -->", currentPtr->id, currentPtr->name );
          currentPtr = currentPtr->nextPtr;
       } // end while
 
-      printf( "%d %s --> NULL\n",currentPtr->id, currentPtr->name );
+      printf( "%d %s -->NULL\n",currentPtr->id, currentPtr->name );
 
    } // end else
 } // end function printList
@@ -176,11 +177,11 @@ void printList_r( LLPtr currentPtr)
       }
 
       while (currentPtr->pPtr != NULL) {
-         printf( "%d %s --> ", currentPtr->id, currentPtr->name );
+         printf( "%d %s -->", currentPtr->id, currentPtr->name );
          currentPtr = currentPtr->pPtr;
       }
 
-      printf( "%d %s --> NULL\n",currentPtr->id, currentPtr->name );
+      printf( "%d %s -->NULL\n",currentPtr->id, currentPtr->name );
        
    }
 }
